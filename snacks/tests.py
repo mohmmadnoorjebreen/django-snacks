460f59551820339844a11c4885c1ea0bb9be0ab6
MOH <mohammadnoormjk1998@gmail.com>
2021-08-29 18:05:38 +0300

    Lab: 26 - Intro to Django

---
+++ snacks/tests.py
@@ -0,0 +1,19 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+# Create your tests here.
+class thingsTests(SimpleTestCase):
+    def test_home_page_status(self):
+            url = reverse('home')
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status(self):
+            url = reverse('about')
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)        
+
+    def test_home_page_templates(self):
+            url = reverse('home')
+            response = self.client.get(url)
+            self.assertTemplateUsed(response, 'home.html')
+            self.assertTemplateUsed(response, 'base.html')
