73a945ae9675b59f0a06bb4c012761c4a9c4669b
MOH <mohammadnoormjk1998@gmail.com>
2021-08-29 18:30:08 +0300

    add about test

--- snacks/tests.py
+++ snacks/tests.py
@@ -12,8 +12,14 @@ class snacksTests(SimpleTestCase):
             response = self.client.get(url)
             self.assertEqual(response.status_code, 200)        
 
-    def test_home_page_templates(self):
+    def test_home_page_home_template(self):
             url = reverse('home')
             response = self.client.get(url)
             self.assertTemplateUsed(response, 'home.html')
+            self.assertTemplateUsed(response, 'base.html')
+         
+    def test_home_page_about_template(self):
+            url = reverse('about')
+            response = self.client.get(url)
+            self.assertTemplateUsed(response, 'about.html')
             self.assertTemplateUsed(response, 'base.html')
